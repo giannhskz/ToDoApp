b3e94c8d8a5d4efe800abc7a163d422197b56dd5
giannhskz <giannhskz@gmail.com>
2023-12-27 14:56:59 +0200

    δημιουργια model todos και καθορισμός relationships

--- app/Livewire/ToDos.php
+++ app/Livewire/ToDos.php
@@ -7,8 +7,17 @@ use Livewire\Component;
 class ToDos extends Component
 {
     public $user;
+    public $toDos;
+
+    public function addToDo(){
+
+    }
     public function render()
     {
         return view('livewire.to-dos');
+    }
+
+    public function mount(){
+        $toDos = $this->user->todos();
     }
 }
